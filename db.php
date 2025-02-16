fb8ad3e4d71cbcc44fd6dc16d8c1649da4f65f5f
Gerardo <gerardo.dev21@hotmail.com>
2025-02-15 23:00:46 -0600

    Primer commit del proyecto. Agregando archivos al repositorio.

---
+++ db.php
@@ -0,0 +1,17 @@
+<!--Aquí voy a generar la conexión con la base de datos usando la bibloteca de mysql 
+mysqli y su función llamada connect -> mysqli_connect()
+Dentro de ésta función le pasaremos los parámetros de nuestra base de datos. -->
+
+<?php
+    $connection = mysqli_connect(
+        'localhost', /*Aquí va el servidor de la DB (puede ser una IP)*/
+        'root', /*Aquí va el usuario de la DB */
+        '', /*Aquí va la contraseña de la DB */
+        'php_mysql_crud' /*Aquí va el nombre de la DB */
+    );
+    /*El script anterior me devuelve un objeto de coleccióno que hay que almacenarlo en una variable */
+    //Verificamos que la FB esté conectada
+    // if(isset($connection)){
+    //     echo "DB is connected!";
+    // }
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php include("db.php")?> <!-- Es importante ésta línea ya que con ella le indicamos 
+a nuestra app que lo primero que cargue al iniciar sea la conexión con la DB -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP MYSQL CRUD</title>
+</head>
+<body>
+    <h1>Hola World</h1>
+    
+</body>
+</html>
